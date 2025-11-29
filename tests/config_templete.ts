0b8e36b1ffcc537252eb8df739f57a8bbc88dc6a
bzls <2395913652@qq.com>
2025-11-29 10:19:47 +0800

    优化提示词

--- tests/config_templete.ts
+++ tests/config_templete.ts
@@ -1,3 +1,4 @@
+//也可以使用自己的大模型api 结构类似于下面，并修改export，由于并不是所有的api请求都一样，如有错误可以修改原文res对象结构
 //豆包
 const politics_exam_config_doubao = {
 
@@ -22,6 +23,6 @@ const politics_exam_config_deepseek = {
 const politics_exam_global_config = {
     PAGE_URL:
         '',//TODO:雨课堂链接
-    PROMPT: '根据选择题和判断题内容给出回复，如果是单选题直接给出大写字母，多选题给出字母字符串用&分割，例如单选题答案是C,只给出“C”,多选题是ABE，给出“A&B&E”，如果是判断题，正确给出‘Y’,错误给出“W”，,除了&和英文字母不要有任何其他符号，如果结果未知不能给出，给出“&”',
+    PROMPT: '根据选择题和判断题内容给出回复，如果是单选题直接给出大写字母，多选题给出字母字符串用&分割，例如单选题答案是C,只给出“C”,多选题是ABE，给出“A&B&E”，如果是判断题，正确给出‘Y’,错误给出“W”，,除了&和英文字母不要有任何其他符号，由于文字来自于OCR，识别率并不一定精准，如果有缺失内容，可以猜测文本内容尽量给出一个正确的答案，如果涉及到法律法规结果未知不能给出，给出“&”',
 }
 export const politics_exam_config = {...politics_exam_config_doubao, ...politics_exam_global_config}
